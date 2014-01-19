fe20f2834fcd5e03cd945a3eae7c2f508ee274b0
Nick Collings <nick@collingo.com>
2014-01-19 17:44:23 +0000

    cleaned up server and added custom domain instructions

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var connect = require('connect'),
     localDir = 'dev',
     builtDir = 'www';
 
+// serve built code
 var built = connect()
 	.use(connect.logger('dev'))
 	.use(connect.static(builtDir))
 	.use(connect.directory(builtDir));
 http.createServer(built).listen(8080);
 
+// serve development code when not in production
 if(!process.env.subdomain) {
 
 	var dev = connect()
@@ -18,21 +20,3 @@ if(!process.env.subdomain) {
 	http.createServer(dev).listen(8081);
 
 }
-
-// // requires node's http module
-// var http = require('http');
-
-// // creates a new httpServer instance
-// http.createServer(function (req, res) {
-// 	// this is the callback, or request handler for the httpServer
-
-// 	// respond to the browser, write some headers so the 
-// 	// browser knows what type of content we are sending
-// 	res.writeHead(200, {'Content-Type': 'text/html'});
-
-// 	// write some content to the browser that your user will see
-// 	res.write('<h1>hello, i know nodejitsu.</h1>');
-
-// 	// close the response
-// 	res.end();
-// }).listen(8080); // the server will listen on port 8080
