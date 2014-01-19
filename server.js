91a907e76778cd44199a42c10fe44c9a77d6e874
Nick Collings <nick@collingo.com>
2014-01-19 14:24:31 +0000

    reverted server to todo base

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-// var connect = require('connect'),
-//     staticDir = 'www',
-//     builtDir = 'dist';
+var connect = require('connect'),
+    localDir = 'www',
+    builtDir = 'dist';
 
-// connect().use(connect.static(staticDir)).listen(8080);
-// connect().use(connect.static(builtDir)).listen(9090);
-
-// requires node's http module
-var http = require('http');
-
-// creates a new httpServer instance
-http.createServer(function (req, res) {
-	// this is the callback, or request handler for the httpServer
-
-	// respond to the browser, write some headers so the 
-	// browser knows what type of content we are sending
-	res.writeHead(200, {'Content-Type': 'text/html'});
-
-	// write some content to the browser that your user will see
-	res.write('<h1>hello, i know nodejitsu and how to deploy via github hooks and codeship integration.</h1>');
-
-	// close the response
-	res.end();
-}).listen(8080); // the server will listen on port 8080
+if(process.env.subdomain) {
+	connect().use(connect.static(builtDir)).listen(8080);
+} else {
+	connect().use(connect.static(localDir)).listen(8080);
+	connect().use(connect.static(builtDir)).listen(8081);
+}
