1453fa9473c19b8ed0db4246b3a4035c2963c88b
Nick Collings <nick@collingo.com>
2014-01-19 16:01:23 +0000

    reverting to example server

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
-var connect = require('connect'),
-	http = require('http'),
-    localDir = 'www',
-    builtDir = 'dist';
+// var connect = require('connect'),
+// 	http = require('http'),
+//     localDir = 'www',
+//     builtDir = 'dist';
 
-var built = connect()
-	.use(connect.logger('dev'))
-	.use(connect.static(builtDir))
-	.use(connect.directory(builtDir));
-http.createServer(built).listen(8080);
+// var built = connect()
+// 	.use(connect.logger('dev'))
+// 	.use(connect.static(builtDir))
+// 	.use(connect.directory(builtDir));
+// http.createServer(built).listen(8080);
 
-if(!process.env.subdomain) {
+// if(!process.env.subdomain) {
 
-	var dev = connect()
-		.use(connect.logger('dev'))
-		.use(connect.static(localDir))
-		.use(connect.directory(localDir));
-	http.createServer(dev).listen(8081);
+// 	var dev = connect()
+// 		.use(connect.logger('dev'))
+// 		.use(connect.static(localDir))
+// 		.use(connect.directory(localDir));
+// 	http.createServer(dev).listen(8081);
 
-}
+// }
+
+// requires node's http module
+var http = require('http');
+
+// creates a new httpServer instance
+http.createServer(function (req, res) {
+  // this is the callback, or request handler for the httpServer
+
+  // respond to the browser, write some headers so the 
+  // browser knows what type of content we are sending
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // write some content to the browser that your user will see
+  res.write('<h1>hello, i know nodejitsu.</h1>');
+
+  // close the response
+  res.end();
+}).listen(8080); // the server will listen on port 8080
