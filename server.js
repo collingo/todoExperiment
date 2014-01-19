41c6e835dbb8c08136d25279f611f8bbbce0bf0a
Nick Collings <nick@collingo.com>
2014-01-19 13:07:26 +0000

    added nodejitsu

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-var connect = require('connect'),
-    staticDir = 'www',
-    builtDir = 'dist';
+// var connect = require('connect'),
+//     staticDir = 'www',
+//     builtDir = 'dist';
 
-connect().use(connect.static(staticDir)).listen(8080);
-connect().use(connect.static(builtDir)).listen(9090);
+// connect().use(connect.static(staticDir)).listen(8080);
+// connect().use(connect.static(builtDir)).listen(9090);
+
+// requires node's http module
+var http = require('http');
+
+// creates a new httpServer instance
+http.createServer(function (req, res) {
+  // this is the callback, or request handler for the httpServer
+
+  // respond to the browser, write some headers so the 
+  // browser knows what type of content we are sending
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // write some content to the browser that your user will see
+  res.write('<h1>hello, i know nodejitsu and how to deploy via github hooks.</h1>');
+
+  // close the response
+  res.end();
+}).listen(8080); // the server will listen on port 8080
--- www/mods/main/main.js
+++ www/mods/main/main.js
@@ -1,10 +1,66 @@
 define([
-	'mods/module/module'
+	'underscore/collections/forEach',
+	'jquery'
 ], function(
-	module
+	_forEach,
+	$
 ) {
 
-	console.log('main started');
-	console.log(module);
+	var data = [{
+		"id": 0,
+		"text": "Todo",
+		"done": false,
+		"items": [{
+			"id": 4,
+			"text": "Clean house",
+			"done": false
+		}]
+	}, {
+		"id": 1,
+		"text": "Work",
+		"done": false,
+		"items": [{
+			"id": 5,
+			"text": "Email client",
+			"done": false
+		}]
+	}, {
+		"id": 2,
+		"text": "Shopping",
+		"done": false,
+		"items": [{
+			"id": 6,
+			"text": "Milk",
+			"done": false
+		}, {
+			"id": 7,
+			"text": "Bread",
+			"done": false
+		}]
+	}, {
+		"id": 3,
+		"text": "Phone Mum",
+		"done": false
+	}];
+
+	var html = "<ul>";
+	_forEach(data, function(item, index) {
+		console.log(item.text);
+		html += "<li><p>"+item.text+"</p>";
+
+		if(item.items) {
+			_forEach(item.items, function(subitem, subindex) {
+				console.log(">", subitem.text);
+			});
+		}
+		html += "</li>";
+	});
+	html += "</ul>";
+
+	$('body').append(html);
+
+	function renderItem(item) {
+
+	}
 
 });
