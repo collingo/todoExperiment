c8624a810a1386de6ac334b2e2d3cef969d64ab1
Nick Collings <nick@collingo.com>
2014-02-15 17:29:23 +0000

    refactored server code to use express3-handlebars middleware

--- server.js
+++ server.js
@@ -1,45 +1,33 @@
 var express = require('express'),
-	handlebars = require('handlebars'),
-    localDir = 'dev',
-    builtDir = 'www';
+    exphbs = require('express3-handlebars'),
+    builtDir = 'www',
+    devDir = 'dev';
 
-var built = express()
-	.use(express.static(builtDir));
-var builtIndex = require('./www/index.hbs');
-built.get('/', function(req, res){
-	var body = builtIndex({id:0});
-	res.setHeader('Content-Type', 'text/html');
-	res.setHeader('Content-Length', Buffer.byteLength(body));
-	res.end(body);
-});
-built.get('/:id', function(req, res){
-	var body = builtIndex({id:req.params.id});
-	res.setHeader('Content-Type', 'text/html');
-	res.setHeader('Content-Length', Buffer.byteLength(body));
-	res.end(body);
-});
-built.listen(8080);
-console.log("Built distribution server started on http://localhost:8080/");
+function setupServer(name, port, directory) {
+	var server = express().use(express.static(directory));
+	server.engine('hbs', exphbs({extname: '.hbs'}));
+	server.set('view engine', 'hbs');
+	server.set('views', __dirname + '/' + directory);
+	server.get('/', function(req, res) {
+		res.render('index', {
+			id: 0
+		});
+	});
+	server.get('/:id', function(req, res) {
+		res.render('index', {
+			id: req.params.id
+		});
+	});
+	server.listen(port);
+	console.log(name+" server started on http://localhost:"+port+"/");
+	return server;
+}
+
+var built = setupServer("Built", 8080, builtDir);
 
 // serve development code when not in production
 if(!process.env.subdomain) {
 
-	var dev = express()
-		.use(express.static(localDir));
-	var devIndex = require('./dev/index.hbs');
-	dev.get('/', function(req, res){
-		var body = devIndex({id:0});
-		res.setHeader('Content-Type', 'text/html');
-		res.setHeader('Content-Length', Buffer.byteLength(body));
-		res.end(body);
-	});
-	dev.get('/:id', function(req, res){
-		var body = devIndex({id:req.params.id});
-		res.setHeader('Content-Type', 'text/html');
-		res.setHeader('Content-Length', Buffer.byteLength(body));
-		res.end(body);
-	});
-	dev.listen(8081);
-	console.log("Local development server started on http://localhost:8081/");
+	var dev = setupServer("Local", 8081, devDir);
 
 }
